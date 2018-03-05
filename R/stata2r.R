386a7a4b2594f1debe2cc2910274d5c15dcdf32c
seanmcraig <sean.craiG@pitt.edu>
2018-03-05 04:28:07 -0500

    Added "generate" and "replace" functions; modification of general variable names; shortening function name.

--- R/stata2r.R
+++ R/stata2r.R
@@ -1,70 +1,210 @@
 #' Translate Stata to R
 #'
 #' This function translates common Stata instructions into R and executes the resulting code. Stata instructions are recorded as a string, which are broken down and interpreted.
-#' @param code Enter code as you would in Stata, surrounded by single quotes.
+#' @param input Enter code as you would in Stata, surrounded by single quotes.
 #' @keywords stata
 #' @import stringr haven
 #' @export
 #' @examples
-#' stata2r('pwd')
+#' s2r('replace x=5 if y==3')
 
-stata2r <- function(
-  code
-){
-  #Load packages
+s2r <- function(input){
+  
+  # (1) FORMATTING THE INPUT STRING
+  
+  # Load general packages
   library(stringr)
-  #Remove all quotation marks from the code string
-  code1 <- gsub('"',"",code)
-  code2 <- gsub("([A-Za-z]),([A-Za-z])", "\\1 , \\2", code1)
-  code3 <- gsub("([A-Za-z]),", "\\1 ,", code2)
-  code4 <- gsub(",([A-Za-z])", ", \\1", code3)
-  code <- code4
-  rm(code1,code2,code3,code4)
-  # Define general objects
-  vstring <- strsplit(code, " ")
-  vstring_df <- data.frame(vstring)
-  colnames(vstring_df)[1] <- "v1"
-  cmd <- as.character(vstring_df$v1[1])
-  loc_comma = which(vstring_df$v1==",")
-  loc_end = NROW(vstring_df)
-  # "cd" Change Directory
+  
+  # Strip internal quotation marks from the input string
+  mod_1 <- gsub('"',"",input)
+  
+  # Ensure that commas are padded on both sides with " "
+  mod_2 <- gsub("([A-Za-z]),([A-Za-z])", "\\1 , \\2", mod_1)
+  mod_3 <- gsub("([A-Za-z]),", "\\1 ,", mod_2)
+  mod_4 <- gsub(",([A-Za-z])", ", \\1", mod_3)
+  
+  
+  # Define the final code string and clean up local variables
+  cstring <- mod_4
+  
+  # Split the code string into a vector
+  cvector <- data.frame(strsplit(cstring, " "))
+  colnames(cvector)[1] <- "v1"
+  
+  # Identify the command
+  if(cvector$v1[1]=="by"){ 
+    return(print('ERROR: "by" is not supported at this time')) 
+  } else if (cvector$v1[1]=="bysort"){ 
+    return(print('ERROR: "bysort" is not supported at this time')) 
+  } else {
+    cmd <- as.character(cvector$v1[1])
+  }
+  
+  # Identify important locations
+  loc_comma <- which(cvector$v1==",")
+  loc_end = NROW(cvector)
+  
+  # (2) COMMAND DICTIONARY
+  
+  # "cd"
   if(cmd == "cd"){
-    path <- paste(vstring_df$v1[2:loc_end], collapse=" ")
+    path <- paste(cvector$v1[2:loc_end], collapse=" ")
     setwd(path)
     verb <- paste("setwd(",path,")")
     print(noquote(verb))
-    rm(path, verb)
   }
-  # "clear" Clear data
+  
+  # "generate"
+  if(is.element(cmd, c("generate","generat","genera","gener","gene","gen","ge","g"))==TRUE){
+    str <- "str"
+    set <- c(1:2045)
+    strset <- paste0(str,set)
+    type <- ifelse(
+      is.element(
+        cvector$v1[2], c("byte","int","long","float","double","str", strset)
+        )==TRUE, cvector$v1[2], "none")
+    loc_newvar <- ifelse(type=="none", 2, 3)
+    
+    # "if" and "in" are currently unsupported
+    loc_if <- which(cvector$v1=="if")
+    loc_in <- which(cvector$v1=="in")
+    if(length(loc_if)>0){return(noquote('ERROR: "if" is unsupported at this time'))}
+    if(length(loc_in)>0){return(noquote('ERROR: "in" is unsupported at this time'))}
+    
+    # Identify the end of the expression
+    loc_stop <- ifelse(length(loc_comma)>0, loc_comma-1, loc_end)
+    expression <- gsub("=","<-",paste0(na.omit(cvector$v1[loc_newvar:loc_stop]),collapse=""))
+    
+    # Generate the variable
+    if(exists('mydata', envir=.GlobalEnv)==FALSE){newframe <-1} else{newframe <- 0}
+    if(newframe==1){mydata <<- data.frame(0)}
+    if(type=="none"){verb <- paste0("mydata$",expression)}
+    
+    if(type!="none"){return(print("ERROR: Specifying data type is not yet supported"))}
+    
+    # Evaluate the command and produce the output
+    eval(parse(text=verb), envir=.GlobalEnv)
+    if(newframe==1){mydata$X0 <<- NULL}
+    print(noquote(verb))
+  }
+  
+  # "clear"
   if(cmd == "clear"){
-    if(exists("mydata", envir = .GlobalEnv)){rm(mydata, envir = .GlobalEnv)}
-    else(return())
+    if(exists("mydata", envir = .GlobalEnv)){
+      rm(mydata, envir = .GlobalEnv)
+    } else(return())
   }
-  # "pwd" Print Working Directing
+  
+  # "pwd"
   if(cmd == "pwd"){
     verb <- "getwd()"
     out <- getwd()
     print(noquote(verb))
-    print(noquote(out))
-    rm(out, verb)
+    print(noquote(out)) 
   }
-  # "use" Load Stata Dataset
-  if(cmd == "use"){
+  
+  # "replace"
+  if(cmd == "replace"){
+    if(exists("mydata")==FALSE){return(noquote("ERROR: stata2r compatible data is not loaded"))}
+    
+    # Find the end of the replace, in, and if expressions
+    loc_if <- which(cvector$v1=="if")
+    loc_in <- which(cvector$v1=="in")
+    
+    if(length(loc_if)>0 & length(loc_in)>0){
+      if(loc_if-loc_in<0){
+        loc_stop <- loc_if-1
+        loc_ifstop <- loc_in-1
+        loc_instop <- ifelse(length(loc_comma)>0, loc_comma-1, loc_end)
+      } else {
+        loc_stop <- loc_in-1
+        loc_instop <- loc_if-1
+        loc_ifstop <- ifelse(length(loc_comma)>0, loc_comma-1, loc_end)
+      }
+    } else if (length(loc_if)>0 & length(loc_in)<1) {
+      loc_stop <- loc_if-1
+      loc_ifstop <- ifelse(length(loc_comma)>0, loc_comma-1, loc_end)
+    } else if (length(loc_if)<1 & length(loc_in)>0) {
+      loc_stop <- loc_in-1
+      loc_instop <- ifelse(length(loc_comma)>0, loc_comma-1, loc_end)
+    } else if (length(loc_comma)>0) {
+      loc_stop <- loc_comma-1
+    } else {
+      loc_stop <- loc_end
+    }
+    
+    # Define the expressions and correct their syntax
+    expression <- gsub("=","<-", paste0(cvector$v1[2:loc_stop], collapse=""))
+    expression_2 <- sub("^[^<]*","",expression)
+    
+    ifrowid <- paste0(loc_if+1,":",loc_ifstop,collapse="")
+    ifverb <- paste0("cvector$v1[",ifrowid,"]", collapse="")
+    inrowid <- paste0(loc_in+1,":",loc_instop,collapse="")
+    inverb <- paste0("cvector$v1[",inrowid,"]", collapse="")
+    if(length(loc_if)>0){if_exp <- eval(parse(text=ifverb))}
+    if(length(loc_in)>0){
+      in_exp <- eval(parse(text=inverb))
+      in_exp <- gsub("f","1",in_exp)
+      in_exp <- gsub("l",NROW(mydata),in_exp)
+      in_exp <- gsub("[^0-9]/[^0-9]","\\1,\\2",in_exp)
+    }
+    if(exists("if_exp")==FALSE){if_exp <- ""}
+    if(exists("in_exp")==FALSE){in_exp <- ""}
+    
+    # Determine the correct command, execute, and return output
+    oldvar <- sub("*=.","", cvector$v1[2])
+    ifvar <- gsub("=.*","",if_exp)
+    ifvar <- gsub("<.*","",ifvar)
+    ifvar <- gsub(">.*","",ifvar)
+    if(length(loc_if)>0 && length(loc_in)>0){
+      in_exp_mod <- sub("([0-9]):([0-9])","c(\\1:\\2)",in_exp)
+      if_exp_mod <- if(grepl("<",if_exp)==TRUE){sub("^[^<]*","",if_exp)} else{if_exp_mod <- if_exp}
+      if_exp_mod <- if(grepl(">",if_exp)==TRUE){sub("^[^>]*","",if_exp)} else{if_exp_mod <- if_exp_mod}
+      if_exp_mod <- if(grepl("=",if_exp)==TRUE 
+                       && grepl(">",if_exp)==FALSE 
+                             && grepl("<",if_exp)==FALSE){sub("^[^=]*","",if_exp)
+                               } else{if_exp_mod <- if_exp_mod}
+        
+      if_test<- paste0("which(mydata$",if_exp,")", collapse="")
+      if_satisfied <- paste0(eval(parse(text=if_test)),sep=",",collapse="")
+      if_satisfied <- paste0(substr(if_satisfied,1,nchar(if_satisfied)-1), collapse="")
+      if_satisfied2 <- paste0("c(",if_satisfied,")")
+      if_set <- eval(parse(text=if_satisfied2))
+      ifin_test <- paste0("which(is.element(",if_satisfied2,",",in_exp_mod,")==TRUE)", collapse="")
+      ifin_satisfied <- eval(parse(text=ifin_test))
+      rowid_test <- paste0(if_satisfied2,"[",ifin_satisfied,"]")
+      rowid <- eval(parse(text=rowid_test))
+      
+      #Error escape
+      if(length(rowid)<1){return("Request produced no real changes")}
+        
+      verb <- paste0("mydata$",oldvar,"[",rowid,"]", expression_2, collapse="")
+    } else if(length(loc_if)>0 && length(loc_in)<1){
+      verb <- paste0("mydata$",oldvar,"[mydata$",if_exp,"]", expression_2, collapse="")
+    } else if(length(loc_if)<1 && length(loc_in)>0){
+      verb <- paste0("mydata$",oldvar,"[",in_exp,"]",expression_2, collapse="")
+    } else {
+      verb <- paste0("mydata$",expression, collapse="")
+    }
+    eval(parse(text=verb), envir=.GlobalEnv)
+    print(noquote(verb))
+  }
+  
+  # "use"
+  if(is.element(cmd, c("use","us","u"))){
     library(haven)
-    loc_using <- which(vstring_df$v1=="using")
-    if(length(loc_using)>0){print('Subsetting with "use" not supported at this time.')}
+    loc_using <- which(cvector$v1=="using")
+    if(length(loc_using)>0){return(print('Subsetting with "use" not supported at this time.'))}
     else{
       loc_stop <- ifelse(length(loc_comma>0),loc_comma-1,loc_end)
-      path <- paste(vstring_df$v1[2:loc_stop],collapse=" ")
+      path <- paste(cvector$v1[2:loc_stop],collapse=" ")
       mydata <<- haven::read_dta(path)
       verb1 <- "library(haven)"
       verb2 <- paste('read_dta("',path,'")',sep="")
       print(noquote(verb1))
       print(noquote(verb2))
-      rm(path,verb1, verb2,loc_stop,loc_using)
     }
   }
   # Remove general objects
-  rm(vstring, vstring_df, cmd, code, loc_comma, loc_end)
 } 
 
--- README.md
+++ README.md
@@ -1,44 +1,63 @@
-stata2r
-======
-This R package contains functions for translating Stata instructions into executable R code as painlessly as possible. The main function is `stata2r()`, which allows the user to execute individual stata instructions.
+# stata2r
 
-## Who Should Use stata2r?
-* Users who want to use common Stata functions but lack a current license.
-* Primary R users who prefer Stata syntax for certain common functions (e.g. data manipulation, or OLS regression).
-* Primary Stata users who would like to learn R gradually while maintaining a familiar workflow.
-* Primary Stata users who need to collaborate with R users.
+## Summary
+*stata2r* is an R package that allows users to translate Stata instructions into executable R code on the fly.
 
-## Usage
-The main function, `stata2r()`, allows the user to a line of Stata code as a string (in single quotes). The package will parse the string, identify corresponding R functions, and output both the translated code and its result. For example,
+## Overview
+*stata2r* in an add-on package for R. It contains functions for parsing strings of Stata code and executing equivalent instructions in R. The main function, `s2r()`, allows the user to execute commmon Stata command-line instructions. The package serves two main groups of users:
+
+1. Primary R users who prefer Stata syntax for simple tasks.
+2. New R users who want a safety net / educational tool.
+
+To better serve the first group, the package aims for robust support of Stata's data manipulation functions and basic regression models. To aid the second group, all functions return the code translation in addition to normal output.
+
+## Installation
+You can install *stata2r* from an R console via the [devtools](https://github.com/hadley/devtools) package.
 ~~~~
-stata2r('use "data.dta", clear')
+install.packages("devtools")
+library(devtools)
+install_github("seanmcraig/stata2r")
 ~~~~
-outputs
+
+## Dependencies
+*stata2r* requires the `haven` package, specifically its `read_dta()` function, in order to read Stata-formatted data files.
+
+## Basic Use
+The main function, `s2r()`, translates a single line of Stata code. Users input stata code as a string in single quotes (`''`). The function will parse the string, identify the corresponding R code, and return the translated code as well as any output. For example, to load a Stata-formatted data file, the user might input the following:
+~~~~
+s2r('use "data.dta", clear')
+~~~~
+*stata2r* will return:
 ~~~~
 mydata <- read_dta("data.dta")
 ~~~~
 
-As Stata only permits one active data source, whereas R permits many active data frames, stata2r executes all data-related functions with reference to a data frame called `mydata`. If you have data loaded in R and you want stata2r to be able to see it, you need to assign your data frame to `mydata`.
-
+As Stata only permits one active data set, Stata syntax does not permit the user interact with a particular R data frame. The workaround is to use one, pre-defined data frame for all Stata instructions. Thus, in *stata2r* all data-related functions use a data frame called `mydata`. If you already have data loaded in R and you want *stata2r* to be able to use it, you need to assign your data frame to `mydata`.
 ~~~~
-mydata <- your.data.frame
+mydata <- otherdata
 ~~~~
 
-## Dependencies
-Package | Notes
---- | ---
-`haven` | `read_dta` function is needed for reading Stata-formatted data files with `use`.
+Please note that *stata2r* assumes that user input is valid Stata code. Invalid code may produce opaque error messages referencing local variables temporarily created by the package functions. If you receive an error, a good first step is to make sure your Stata input contains no errors.
 
-## Current functionality
-#### stata2r is compatible with the following Stata commands:
+A list of Stata commands that work with *stata2r* appears at the bottom of this README.
 
-Command | Description/Notes
---- | ---
-`cd`| Change working directory.
-`clear`| Clear data from memory.
-`pwd`| Print working directory.
 
-#### stata2r is partially compatible with the following Stata commands:
-Command | Description/Notes
+## Supported Stata Commands
+The following Stata commands are at least partially supported:
+Command | Notes
 --- | ---
-`use` | Loading full `.dta` is currently supported (e.g., `use "data.dta"`). Loading subsets of data is not supported (e.g., `use x y using "data.dta"`). Loading Stata variable labels is not supported; the command functions as if the user specifies the `nolabel` option in Stata.
+`cd` | Fully supported.
+`generate` | Partially supported: "if" and "in" arguments are not supported; missing values are not supported.
+`clear` | Fully supported.
+`pwd` | Fully supported.
+`replace` | Mostly supported: option "nopromote" is not supported.
+`use` | Partially supported: loading subsets not supported; loading Stata value labels is not supported (command functions as if `nolabel` option was used)
+
+## Package Author
+Sean Craig ([sean.craig@pitt.edu](mailto:sean.craig@pitt.edu))
+
+## License
+[GNU GPLv3](https://www.gnu.org/licenses/gpl-3.0.txt) (GNU General Public License, Version 3)
+
+
+
